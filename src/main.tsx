e2ab099ffb43865fe2f1bc58a56f9715db5057fe
levmn <levimncontato@gmail.com>
2024-09-03 22:16:24 -0300

    Atualizando estrutura inicial

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { GlobalStyles } from './styles/GlobalStyles'
 import App from './App.tsx'
-import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <GlobalStyles />
     <App />
   </StrictMode>,
 )
