52ee8bf0d1d92635b7deb4085c366235dfb56e06
levmn <levimncontato@gmail.com>
2024-09-10 23:20:02 -0300

    Adicionando media queries

--- src/components/Footer/Footer.style.ts
+++ src/components/Footer/Footer.style.ts
@@ -10,6 +10,33 @@ export const StyledFooter = styled.footer`
     color: #FFFFFF;
     position: relative;
     bottom: 0;
+
+    /* Media Queries */
+    @media (max-width: 575px) {
+        height: 150px;
+        padding: 0 20px;
+        align-items: center;
+    }
+
+    @media (min-width: 576px) and (max-width: 767px) {
+        height: 150px;
+        padding: 0 20px;
+        align-items: center;
+    }
+
+    @media (min-width: 768px) and (max-width: 991px) {
+        height: 150px;
+        padding: 0 20px;
+        align-items: center;
+    }
+
+    @media (min-width: 992px) and (max-width: 1199px) {
+        
+    }
+
+    @media (min-width: 1200px) {
+
+    }
 `;
 
 export const StyledDiv = styled.div`
@@ -20,12 +47,94 @@ export const StyledDiv = styled.div`
     #logo-footer {
         width: 300px;
     }
+
+    /* Media Queries */
+    @media (max-width: 575px) {
+        #logo-footer {
+            width: 200px;
+        }
+
+        p {
+            font-size: 0.7rem;
+        }
+    }
+
+    @media (min-width: 576px) and (max-width: 767px) {
+        #logo-footer {
+            width: 250px;
+        }
+
+        p {
+            font-size: 0.8rem;
+        }
+    }
+
+    @media (min-width: 768px) and (max-width: 991px) {
+        #logo-footer {
+            width: 250px;
+        }
+
+        p {
+            font-size: 0.9rem;
+        }
+    }
+
+    @media (min-width: 992px) and (max-width: 1199px) {
+        #logo-footer {
+            width: 250px;
+        }
+
+        p {
+            font-size: 1rem;
+        }
+    }
+
+    @media (min-width: 1200px) {
+        #logo-footer {
+            width: 300px;
+        }
+    }
 `;
 
 export const StyledRedesSociais = styled.div`
     display: flex;
     flex-direction: column;
     gap: 10px;
+
+    p {
+        font-size: 20px;
+    }
+
+    /* Media Queries */
+    @media (max-width: 575px) {
+        p {
+            display: none;
+        }
+    }
+
+    @media (min-width: 576px) and (max-width: 767px) {
+        p {
+            display: none;
+        }
+    }
+
+    @media (min-width: 768px) and (max-width: 991px) {
+        p {
+            font-size: 15px;
+        }
+    }
+
+    @media (min-width: 992px) and (max-width: 1199px) {
+        p {
+            font-size: 17px;
+        }
+    }
+
+    @media (min-width: 1200px) {
+        p {
+            font-size: 20px;
+        }
+    }
 `;
 
 export const StyledIcon = styled.div`
@@ -36,5 +145,36 @@ export const StyledIcon = styled.div`
         color: #FFFFFF;
         font-size: 35px;
         display: inline-block;
+    }
+
+  /* Media Queries */
+    @media (max-width: 575px) {
+        a {
+            font-size: 35px;
+        }
+    }
+
+    @media (min-width: 576px) and (max-width: 767px) {
+        a {
+            font-size: 35px;
+        }
+    }
+
+    @media (min-width: 768px) and (max-width: 991px) {
+        a {
+            font-size: 35px;
+        }
+    }
+
+    @media (min-width: 992px) and (max-width: 1199px) {
+        a {
+            font-size: 35px;
+        }
+    }
+
+    @media (min-width: 1200px) {
+        a {
+            font-size: 35px;
+        }
     }
 `;
--- src/components/Navbar/Navbar.style.ts
+++ src/components/Navbar/Navbar.style.ts
@@ -6,6 +6,26 @@ export const StyledNavbar = styled.nav`
     align-items: center;
     padding: 5px;
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+
+    @media (max-width: 575px) {
+
+    }
+
+    @media (min-width: 576px) and (max-width: 767px) {
+
+    }
+
+    @media (min-width: 768px) and (max-width: 991px) {
+        
+    }
+
+    @media (min-width: 992px) and (max-width: 1199px) {
+        
+    }
+
+    @media (min-width: 1200px) {
+
+    }
 `;
 
 export const StyledDiv = styled.div`
@@ -21,6 +41,27 @@ export const StyledDiv = styled.div`
 
 export const StyledLogo = styled.img`
     width: 300px;
+
+    /* Media Queries */
+    @media (max-width: 575px) {
+        width: 220px;
+    }
+
+    @media (min-width: 576px) and (max-width: 767px) {
+        width: 250px;
+    }
+
+    @media (min-width: 768px) and (max-width: 991px) {
+        width: 250px;
+    }
+
+    @media (min-width: 992px) and (max-width: 1199px) {
+        width: 250px;
+    }
+
+    @media (min-width: 1200px) {
+        width: 300px;
+    }
 `;
 
 export const StyledList = styled.ul`
@@ -37,6 +78,33 @@ export const StyledListItem = styled.li`
     & a {
         text-decoration: none;
         color: #0f5a6c;
+    }
+
+    /* Media Queries */
+    @media (max-width: 575px) {
+        a {
+            display: none;
+        }
+    }
+
+    @media (min-width: 576px) and (max-width: 767px) {
+        a {
+            display: none;
+        }
+    }
+
+    @media (min-width: 768px) and (max-width: 991px) {
+        a {
+            display: none;
+        }
+    }
+
+    @media (min-width: 992px) and (max-width: 1199px) {
+
+    }
+
+    @media (min-width: 1200px) {
+
     }
 `;
 
@@ -66,8 +134,84 @@ export const StyledButton = styled.a`
 
     & p {
         color: #0f5a6c;
-        font-size: 1rem;
+        font-size: 1.2rem;
         font-weight: 500;
         text-align: center;
+    }
+
+    /* Media Queries */
+    @media (max-width: 575px) {
+        width: 100px;
+        height: 45px;
+
+        & #user {
+            width: 30px;
+            height: 30px;
+            margin-right: 10px;
+        }
+
+        & p {
+            font-size: 0.9rem;
+        }
+    }
+
+    @media (min-width: 576px) and (max-width: 767px) {
+        width: 100px;
+        height: 45px;
+
+        & #user {
+            width: 30px;
+            height: 30px;
+            margin-right: 10px;
+        }
+
+        & p {
+            font-size: 0.9rem;
+        }
+    }
+
+    @media (min-width: 768px) and (max-width: 991px) {
+        width: 120px;
+        height: 45px;
+
+        & #user {
+            width: 30px;
+            height: 30px;
+            margin-right: 10px;
+        }
+
+        & p {
+            font-size: 1.2rem;
+        }
+    }
+
+    @media (min-width: 992px) and (max-width: 1199px) {
+        width: 130px;
+        height: 50px;
+
+        & #user {
+            width: 35px;
+            height: 35px;
+            margin-right: 10px;
+        }
+
+        & p {
+            font-size: 1.2rem;
+        }
+    }
+
+    @media (min-width: 1200px) {
+        width: 130px;
+        height: 50px;
+
+        & #user {
+            width: 35px;
+            height: 35px;
+            margin-right: 10px;
+        }
+
+        & p {
+            font-size: 1.2rem;
+        }
     }
 `;
