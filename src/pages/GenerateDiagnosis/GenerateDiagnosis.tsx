6ed1d0cc985e7ec49d0cc2ee18149b3162146740
Caio Liang <caioliang2@gmail.com>
2024-09-14 19:19:22 -0300

    Adicionando CSS no style GenerateDiagnosis

---
+++ src/pages/GenerateDiagnosis/GenerateDiagnosis.tsx
@@ -0,0 +1,18 @@
+import React, { useState} from 'react';
+import { Footer } from "../../components/Footer/Footer";
+import { Layout } from "../../components/Layout/Layout";
+import { Navbar } from "../../components/Navbar/Navbar";
+import { IoIosArrowBack } from "react-icons/io";
+
+export default function GenerateDiagnosis () {
+    
+   
+
+    return (
+        <>
+        <Navbar type="logged" />
+        
+        </>
+    );
+};
+
